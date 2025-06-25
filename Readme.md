d7db1bc11991c81eab40f2c45ffdb779431cacda
Pawel Wujczyk <pwujczyk@gmail.com>
2025-06-25 18:51:33 +0200

    add graphql

--- Readme.md
+++ Readme.md
@@ -2,3 +2,14 @@
 .\gradlew.bat wrapper
 .\gradlew.bat bootrun
 ```
+
+Open [page](http://localhost:8080/graphiql?path=/graphql)
+
+invoke
+```
+query getPersonQuery{
+  getPerson {
+    firstName,
+    lastName
+  }
+}
--- src/main/java/top/productivitytools/habits/api/entities/Person.java
+++ src/main/java/top/productivitytools/habits/api/entities/Person.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "person")
 public class Person {
